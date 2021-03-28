4b532cac1efc72bfc7df3dfd3e8c93f1aa74122b
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-03-28 15:25:44 -0300

    footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import Hero from '../Hero/Hero';
 import Cards from '../Cards/Cards';
+import Footer from '../Footer/Footer';
 
 function Home () {
     return (
         <>
             <Hero />
             <Cards />
+            <Footer />
         </>
     )
 }
